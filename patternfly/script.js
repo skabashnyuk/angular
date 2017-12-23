7a561c6d5e753a15bb7b8968a91f1d5f7f025798
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-23 22:48:49 +0200

    Test not working

---
+++ patternfly/script.js
@@ -0,0 +1,35 @@
+angular.module('patternfly.navigation').controller('applicationLauncherController', ['$scope', '$window',
+  function ($scope, $window) {
+    $scope.imagePath = $window.IMAGE_PATH || "img";
+    $scope.navigationItems = [
+      {
+        title: "Recteque",
+        href: "#/ipsum/intellegam/recteque",
+        tooltip: "Launch the Function User Interface",
+        iconClass: "pficon-storage-domain"
+      },
+      {
+        title: "Suavitate",
+        href: "#/ipsum/intellegam/suavitate",
+        tooltip: "Launch the Function User Interface",
+        iconClass: "pficon-build"
+      },
+      {
+        title: "Lorem",
+        href: "#/ipsum/intellegam/lorem",
+        tooltip: "Launch the Function User Interface",
+        iconClass: "pficon-domain"
+      },
+      {
+        title: "Home",
+        href: "#/ipsum/intellegam/home",
+        tooltip: "Launch the Function User Interface",
+        iconClass: "pficon-home"
+      }
+    ];
+ 
+    $scope.label = 'Application Launcher';
+    $scope.isDisabled = false;
+    $scope.isList = false;
+    $scope.hiddenIcons = 'false';
+  }]);
