23b3f5ab5ea4cb5a816ba4562e1a509a1bf73451
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-25 22:42:40 +0200

    Page 123

--- JavaScriptPrimer/primer.ts
+++ JavaScriptPrimer/primer.ts
@@ -1,7 +1,10 @@
 import { Name, WeatherLocation } from "./modules/NameAndWeather";
+import {Name as OtherName} from "./modules/DuplocateName"
 
 let name = new Name("Adam", "Freeman");
 let loc = new WeatherLocation("raining","London");
+let other = new OtherName()
 
 console.log(name.nameMessage);
 console.log(loc.wetherMessage);
+console.log(other.message);
