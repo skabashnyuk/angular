721c6ba2b6d2dd7be591dabb0781415cf2425529
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-25 22:50:21 +0200

    Page 125

--- JavaScriptPrimer/primer.ts
+++ JavaScriptPrimer/primer.ts
@@ -1,10 +1,11 @@
 import { Name, WeatherLocation } from "./modules/NameAndWeather";
 import {Name as OtherName} from "./modules/DuplocateName"
+import { TempConverter } from "./modules/tempConverter";
 
 let name = new Name("Adam", "Freeman");
 let loc = new WeatherLocation("raining","London");
 let other = new OtherName()
-
+let cTemp = TempConverter.convertFtoC("38");
 console.log(name.nameMessage);
 console.log(loc.wetherMessage);
-console.log(other.message);
+console.log(`The temp is ${cTemp}`);
