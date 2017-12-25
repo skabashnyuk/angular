bb3624c655f86cfc2b581ec7bc1c05dda9fe1107
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-25 14:40:55 +0200

    Page 100

--- JavaScriptPrimer/primer.js
+++ JavaScriptPrimer/primer.js
@@ -1,1 +1,13 @@
-console.log("Hello12");
+var messageFunction = function (name, weather) {
+    var message = "Hello, Adam ";
+    if (weather == "sunny") {
+        var message_1 = "It is a nice day";
+        console.log(message_1);
+    }
+    else {
+        var message_2 = "It is " + weather + " today";
+        console.log(message_2);
+    }
+    console.log(message);
+};
+messageFunction("Adam", "raining");
--- JavaScriptPrimer/primer.ts
+++ JavaScriptPrimer/primer.ts
@@ -1,1 +1,12 @@
-console.log("Hello12");
+let messageFunction = function (name, weather) {
+    let message = "Hello, Adam ";
+    if (weather == "sunny") {
+        let message = "It is a nice day";
+        console.log(message);
+    } else {
+        let message = "It is " + weather + " today";
+        console.log(message);
+    }
+    console.log(message);
+}
+messageFunction("Adam", "raining")
