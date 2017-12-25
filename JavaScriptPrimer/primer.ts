c72ba2558f40ab33797e5ac44c340ac7caae736a
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-12-25 15:31:53 +0200

    Page 118

--- JavaScriptPrimer/primer.ts
+++ JavaScriptPrimer/primer.ts
@@ -1,13 +1,20 @@
 class MyClass {
     constructor(name, weather){
         this.name = name;
-        this.weather = weather;
+        this._weather = weather;
     }
+
+    set weather(value){
+        this._weather = value;
+    }
+    get weather(){
+        return `Today is ${this._weather}`
+    }
+
     printMessage (){
         console.log(`Hello ${this.name} .`);
-        console.log(`Today is  ${this.weather}.`);
+        console.log(this.weather);
     }
 }
-
 let myData = new  MyClass("Adam", "sunny");
 myData.printMessage();
