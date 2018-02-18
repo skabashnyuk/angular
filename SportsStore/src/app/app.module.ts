0ca2cc25be5dd5fcbcae9771c99abefb98fe0e66
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-02-18 23:35:26 +0200

    Page 150

--- SportsStore/src/app/app.module.ts
+++ SportsStore/src/app/app.module.ts
@@ -1,10 +1,10 @@
 ﻿import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
-//import { StoreModule } from "./store/store.module";
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, StoreModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
